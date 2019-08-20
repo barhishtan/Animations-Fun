14ff6d8de16516715c7ec9768a9fcd48c58095c9
Artur Sokolov <barhishtan@gmail.com>
2019-08-20 10:46:46 +0200

    Initial Commit

---
+++ Animations Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations Fun
+//
+//  Created by Artur Sokolov on 20/08/2019.
+//  Copyright © 2019 Artur Sokolov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
